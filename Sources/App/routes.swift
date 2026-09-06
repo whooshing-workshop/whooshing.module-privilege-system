3b95ba948e038410ea3758203fa9c244c8c0a8f0
ChenLin Wang <wangchenlin2001@icloud.com>
2026-09-06 16:17:42 +0800

    V0.0.3.14 更新依赖

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,10 +8,10 @@ func routes(_ nexus: Nexus<VaporTube>) throws {
     let dataRouter = nexus.tube.app.grouped("data")
     try dataRouter.register(collection: DataController())
     
-    let inlineProtected = nexus.tube.app.grouped("inline").grouped(ServiceValidator(), ServiceValidator.Identifier.guardMiddleware())
+    let inlineProtected = nexus.tube.app.inlineProtectGrouped()
     try inlineProtected.register(collection: ArbitrateController())
     
-    let apiProtected = nexus.tube.app.grouped("api").apiProtectGrouped(for: .main, in: nexus)
+    let apiProtected = nexus.tube.app.apiProtectGrouped(for: .main, in: nexus)
     try apiProtected.register(collection: PrivilegeController())
     try apiProtected.register(collection: ApiAccountController())
     apiProtected.get("test") { req in
