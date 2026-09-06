671a3e12fc05afc259f5d7151f8624d5d728584d
ChenLin Wang <wangchenlin2001@icloud.com>
2026-09-06 20:02:49 +0800

    V1.0.0.1 更新依赖

--- Package.swift
+++ Package.swift
@@ -20,9 +20,9 @@ let package = Package(
         // ⭐️ Vapor 管道通讯模块
         .package(url: "https://github.com/whooshing-workshop/whooshing.tube-vapor", from: "1.0.0"),
         // 📁 Whooshing 文件加密系统模块驱动
-        .package(url: "https://github.com/whooshing-workshop/whooshing.driver-file-storage.git", from: "1.1.2"),
+        .package(url: "https://github.com/whooshing-workshop/whooshing.driver-file-storage.git", from: "1.1.3"),
         // 🪩 Whooshing 权限系统模块驱动
-        .package(url: "https://github.com/whooshing-workshop/whooshing.driver-privilege-system.git", from: "1.0.9"),
+        .package(url: "https://github.com/whooshing-workshop/whooshing.driver-privilege-system.git", from: "1.0.8"),
     ],
     targets: [
         .executableTarget(
