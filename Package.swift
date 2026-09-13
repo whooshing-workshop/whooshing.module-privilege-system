113c6d5754cba60c8840551c918622920efd94c8
ChenLin Wang <wangchenlin2001@icloud.com>
2026-09-13 19:39:53 +0800

    V1.0.0.5 更新依赖

--- Package.swift
+++ Package.swift
@@ -22,10 +22,7 @@ let package = Package(
         // 📁 Whooshing 文件加密系统模块驱动
         .package(url: "https://github.com/whooshing-workshop/whooshing.driver-file-storage.git", from: "1.1.3"),
         // 🪩 Whooshing 权限系统模块驱动
-        .package(url: "https://github.com/whooshing-workshop/whooshing.driver-privilege-system.git", branch: "develop"),
-//        .package(path: "/Users/clwang/GitHub/whooshing.driver-privilege-system"),
-        
-        .package(path: "/Users/clwang/GitHub/whooshing.toolbox-privilege-system")
+        .package(url: "https://github.com/whooshing-workshop/whooshing.driver-privilege-system.git", from: "1.0.9")
     ],
     targets: [
         .executableTarget(
